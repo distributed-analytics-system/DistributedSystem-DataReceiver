1da69e334c997a70dd5f0fe149e88474ec5a17ae
Vahagn Saribekyan <Vahag>
2021-09-09 17:24:42 +0400

    Adding docker file

--- src/datastore/datastore.js
+++ src/datastore/datastore.js
@@ -1,5 +1,15 @@
+const config = require('../config');
+const { environments } = require('../constants');
+
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+
+const s3Config = {};
+if (config.nodeEnv === environments.dev) {
+  s3Config.endpoint = process.env.AWS_ENDPOINT;
+  s3Config.s3ForcePathStyle = true;
+}
+
+const s3 = new AWS.S3(s3Config);
 
 const write = (options) => {
   return new Promise((resolve, reject) => {
